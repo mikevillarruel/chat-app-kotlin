af370c3be0b720653c0e2c850be467b6f4f19902
Michael Villarruel <alejovillarruel@gmail.com>
2021-11-03 23:41:18 -0500

    text changed listener added to input message and design update of input message and new messages alert

--- app/src/main/java/com/example/chatapp/ui/fragments/IndividualChatFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/fragments/IndividualChatFragment.kt
@@ -1,12 +1,35 @@
 package com.example.chatapp.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.chatapp.R
+import com.example.chatapp.core.hide
+import com.example.chatapp.core.show
+import com.example.chatapp.databinding.FragmentIndividualChatBinding
 
 class IndividualChatFragment : Fragment(R.layout.fragment_individual_chat) {
+
+    lateinit var binding: FragmentIndividualChatBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentIndividualChatBinding.bind(view)
+
+        binding.txtMessage.addTextChangedListener { text: Editable? ->
+            if (text.toString().isNullOrEmpty()) {
+                binding.btnMicrophone.show()
+                binding.btnCamera.show()
+                binding.btnSend.hide()
+            } else {
+                binding.btnMicrophone.hide()
+                binding.btnCamera.hide()
+                binding.btnSend.show()
+            }
+        }
+
     }
 }
