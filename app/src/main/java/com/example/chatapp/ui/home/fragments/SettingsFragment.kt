fa5aa7e161475e38cf46197428e713c72cea49ae
Michael Villarruel <alejovillarruel@gmail.com>
2021-11-16 23:15:08 -0500

    sign out and some design changes

--- app/src/main/java/com/example/chatapp/ui/home/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/home/fragments/SettingsFragment.kt
@@ -1,12 +1,31 @@
 package com.example.chatapp.ui.home.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.chatapp.R
+import com.example.chatapp.databinding.FragmentSettingsBinding
+import com.example.chatapp.ui.auth.AuthActivity
+import com.example.chatapp.ui.home.MainActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private lateinit var binding: FragmentSettingsBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSettingsBinding.bind(view)
+
+        binding.btnSignOut.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(requireContext(), AuthActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
     }
 }
