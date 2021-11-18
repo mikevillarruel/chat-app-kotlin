b5e424e01d5076d241fe2bcf8385cbc5b30d1aed
Michael Villarruel <alejovillarruel@gmail.com>
2021-11-17 20:27:37 -0500

    calls fragment removed and avoid adding current user in user list

--- app/src/main/java/com/example/chatapp/data/remote/home/HomeDataSource.kt
+++ app/src/main/java/com/example/chatapp/data/remote/home/HomeDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.chatapp.data.remote.home
 
 import com.example.chatapp.data.model.User
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
@@ -8,13 +9,16 @@ import kotlinx.coroutines.tasks.await
 class HomeDataSource {
 
     private val db by lazy { Firebase.firestore }
+    private val auth by lazy { Firebase.auth }
 
     suspend fun getUsers(): List<User> {
         val querySnapshot = db.collection("user").get().await()
         val usersList = mutableListOf<User>()
         querySnapshot.forEach { user ->
-            user.toObject(User::class.java)?.let { newUser ->
-                usersList.add(newUser)
+            if (user.id != auth.currentUser?.uid) {
+                user.toObject(User::class.java)?.let { newUser ->
+                    usersList.add(newUser)
+                }
             }
         }
 
